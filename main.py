f83a3c95334d347059ab607f8584f3c950ccf536
Ss Hemanth <sshemanthbollepalli@gmail.com>
2024-02-01 21:49:46 +0530

    main.py

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ st.sidebar.image("./assets/images/banner.jpeg",use_column_width=True)
 st.sidebar.title("Analyze:")
 st.sidebar.markdown(
     "This app is use to analyze your WhatsApp Chat using the exported text file 📁.")
-st.sidebar.markdown('<b>Prem Chandra Singh</b>\
-                <a href = "https://github.com/pcsingh/WhatsApp-Chat-Analyzer/" ><img src = "https://img.shields.io/badge/Author-@pcsingh-gray.svg?colorA=gray&colorB=dodgerblue&logo=github"/>\
+st.sidebar.markdown('<b>Sri Sai Hemanth Bollepalli</b>\
+                <a href = "https://github.com/pcsingh/WhatsApp-Chat-Analyzer/" ><img src = "https://img.shields.io/badge/Author-@sshemanth-gray.svg?colorA=gray&colorB=dodgerblue&logo=github"/>\
                 <a/>', unsafe_allow_html=True)
 
 st.sidebar.markdown('**How to export chat text file?**')
